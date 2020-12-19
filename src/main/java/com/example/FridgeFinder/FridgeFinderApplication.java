4ece9886df134b60b4796f2f29ec41a054d7ebb6
Ryan <rijid356@gmail.com>
2020-12-19 14:59:32 -0600

    Initial commit

---
+++ src/main/java/com/example/FridgeFinder/FridgeFinderApplication.java
@@ -0,0 +1,13 @@
+package com.example.FridgeFinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FridgeFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FridgeFinderApplication.class, args);
+	}
+
+}
